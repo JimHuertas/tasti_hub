3a5596837c4675ac0dcc463e8743b4da04d89105
JimHuertas <live.land18@gmail.com>
2021-12-16 16:59:35 -0500

    performance crud

--- bd/crud.php
+++ bd/crud.php
@@ -81,6 +81,18 @@ switch($opcion){
         $resultado->execute();
         $data=$resultado->fetchAll(PDO::FETCH_ASSOC);       
         break; 
+    case 8:
+        $consulta = "SELECT * FROM consulta";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();        
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+        break;
+    case 9:        
+        $consulta = "DELETE FROM consulta WHERE id='$id' ";		
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();                           
+        break;
+
 }
 
 print json_encode($data, JSON_UNESCAPED_UNICODE);//envio el array final el formato json a AJAX
